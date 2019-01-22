dbd3116d8fb6f6fb694c172c08b1545894c54265
Mark McCauley <43422703+markmccauley@users.noreply.github.com>
2019-01-22 10:42:58 -0600

    Add files via upload

---
+++ 00/carrots.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, p;
+	string junk;
+	cin >> n;
+	cin >> p;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> junk;
+	}	
+
+	cout << p << endl;
+}
